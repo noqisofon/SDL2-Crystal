987bdbb1d6879cad352446a4ed7bff5ddc42595d
Ned Rihine <ned.rihine@gmail.com>
2022-12-15 22:37:17 +0900

    add spec item

--- spec/sdl2_spec.cr
+++ spec/sdl2_spec.cr
@@ -1,13 +1,28 @@
 require "./spec_helper"
 
 describe "SDL2" do
-  # TODO: Write tests
+  it "Returns 0 if initialization succeeds" do
+    begin
+      answer = LibSDL2.init(LibSDL2::INIT_EVERYTHING)
 
-  it "sanity check" do
-    answer = LibSDL2.init( LibSDL2::INIT_EVERYTHING )
+      answer.should eq 0
+    ensure
+      LibSDL2.quit
+    end
+  end
 
-    answer.should eq 0
+  it "Check for subsystems successfully initialized by was_init" do
+    begin
+      answer = LibSDL2.init(LibSDL2::INIT_EVERYTHING)
 
-    LibSDL2.quit
+      sybsystem_init = LibSDL2.was_init(LibSDL2::INIT_EVERYTHING)
+
+      (sybsystem_init | LibSDL2::INIT_TIMER).should_not eq 0
+      (sybsystem_init | LibSDL2::INIT_AUDIO).should_not eq 0
+      (sybsystem_init | LibSDL2::INIT_VIDEO).should_not eq 0
+      (sybsystem_init | LibSDL2::INIT_GAMECONTROLLER).should_not eq 0
+    ensure
+      LibSDL2.quit
+    end
   end
 end
