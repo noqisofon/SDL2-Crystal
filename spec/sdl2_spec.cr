7fc9a6021e63063eb2de4e3d1a143b876869d0ca
Ned Rihine <ned.rihine@gmail.com>
2022-12-15 21:05:08 +0900

    add spec code

--- spec/sdl2_spec.cr
+++ spec/sdl2_spec.cr
@@ -3,7 +3,11 @@ require "./spec_helper"
 describe "SDL2" do
   # TODO: Write tests
 
-  it "works" do
-    false.should eq(true)
+  it "sanity check" do
+    answer = LibSDL2.init( LibSDL2::INIT_EVERYTHING )
+
+    answer.should_not eq 0
+
+    LibSDL2.quit
   end
 end
